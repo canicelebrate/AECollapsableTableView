7d8ea6dbda5167b696c8826e4f75e5b11a507cc5
William Wang <canicelebrate@gmail.com>
2016-09-28 00:18:31 +0800

    Initial Commit

---
+++ AECollapsableTableViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AECollapsableTableViewDemo
+//
+//  Created by WangLin on 16/9/28.
+//  Copyright © 2016年 amberease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AECollapsableTableViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AECollapsableTableViewDemo
+//
+//  Created by WangLin on 16/9/28.
+//  Copyright © 2016年 amberease. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
